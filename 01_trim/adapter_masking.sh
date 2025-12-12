02aad6e593e4cad3d7c165404050989f4b3429e6
Alice Fornasiero <fornasiero.ali@gmail.com>
2025-12-12 16:34:00 +0100

    Update adapter_masking.sh

--- 01_trim/adapter_masking.sh
+++ 01_trim/adapter_masking.sh
@@ -3,8 +3,8 @@
 #SBATCH --cpus-per-task=32
 #SBATCH --partition=batch
 #SBATCH --job-name cutadap
-#SBATCH -o cutadap.%J.out
-#SBATCH -e cutadap.%J.err
+#SBATCH -o logs/cutadap.%J.out
+#SBATCH -e logs/cutadap.%J.err
 #SBATCH --time=20:00:00
 #SBATCH --mem=20G
 
@@ -12,10 +12,10 @@
 
 # ----------------------------------------------------------------------- #
 # Make sure you update input and output folder path
-# INFOLDER is the path to the folder containing the raw fastq files
-# OUTFOLDER is the path to the output folder
-INFOLDER="ENTER_INPUT_DIRECTORY_NAME_AND_PATH"
-OUTFOLDER="ENTER_OUTPUT_DIRECTORY_NAME AND_PATH"
+# FASTQ_FOLDER is the path to the folder containing the raw fastq files
+# PROJECT_FOLDER is the path to the project folder
+FASTQ_FOLDER="ENTER_INPUT_DIRECTORY_NAME_AND_PATH"
+PROJECT_FOLDER="ENTER_OUTPUT_DIRECTORY_NAME AND_PATH"
 
 # Set variables for Illumina Truseq adapter sequence masking
 adapter1=AGATCGGAAGAGCACACGTCTGAACTCCAGTCA
@@ -24,7 +24,7 @@ n_cores=32
 # ----------------------------------------------------------------------- #
 
 # Generate a list of sample IDs using the names of the fastq.gz files in the initial data directory
-cd ${INFOLDER}
+cd ${FASTQ_FOLDER}
 if [ -f "sample_list.txt" ]; then
     rm sample_list.txt
 fi
@@ -33,10 +33,8 @@ for fastq_file in *_R1.fastq.gz; do
 done >> sample_id_list.txt
 
 # Create output directory and subdirectories
-TRIM_DIR="${OUTFOLDER}/01_trim"
-LOG_DIR="${OUTFOLDER}/01_trim/logs"
+TRIM_DIR="${PROJECT_FOLDER}/01_trim"
 mkdir -p ${TRIM_DIR}
-mkdir -p ${LOG_DIR}
 
 # Change directory to trimming directory
 cd $TRIM_DIR
@@ -46,10 +44,10 @@ module purge
 module load cutadapt/4.3
 
 # Generate the array of slurms
-IFS=$'\n' read -d '' -r -a lines < ${INFOLDER}/sample_id_list.txt
+IFS=$'\n' read -d '' -r -a lines < ${FASTQ_FOLDER}/sample_id_list.txt
 ID=${lines[${SLURM_ARRAY_TASK_ID}]}
 
-# Launch cutadapt
+# Launch Cutadapt
 cutadapt --cores=$n_cores \
 --nextseq-trim=20 \
 -a ${adapter1} \
@@ -58,4 +56,4 @@ cutadapt --cores=$n_cores \
 -o ${ID}_cutadapt_1.fastq.gz \
 -p ${ID}_cutadapt_2.fastq.gz \
 --action=mask \
-${INFOLDER}/${ID}_R1.fastq.gz ${INFOLDER}/${ID}_R2.fastq.gz > logs/${ID}_cutadapt.log
+${FASTQ_FOLDER}/${ID}_R1.fastq.gz ${FASTQ_FOLDER}/${ID}_R2.fastq.gz > logs/${ID}_cutadapt.log
