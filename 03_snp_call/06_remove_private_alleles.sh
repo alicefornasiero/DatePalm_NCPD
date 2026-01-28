be00b91f77ad90ec28fa77bbc0bb34bad79246c3
Alice Fornasiero <fornasiero.ali@gmail.com>
2026-01-28 10:37:32 +0100

    Update script to use VCF_PREFIX for file handling

--- 03_snp_call/06_remove_private_alleles.sh
+++ 03_snp_call/06_remove_private_alleles.sh
@@ -11,10 +11,12 @@
 # Remove private alleles by excluding loci genotyped in one individual (i.e. singletons (1 HET) or doubletons (1 HOMO ALT))
 
 # ----------------------------------------------------------------------- #
-# Make sure you update input and output folder path
-# VCF_FULL_PATH_NAME is the full path and file name of the vcf file to be filtered, including extension
+# PROJECT_FOLDER is the path to the project folder
+# VCF_PREFIX is the prefix name of the GVCF file
+# File must have .vcf.gz extension
 
-VCF_FULL_PATH_NAME="ENTER_VCF_FULL_PATH_AND_FILE_NAME"
+PROJECT_FOLDER="ENTER_OUTPUT_DIRECTORY_PATH"
+VCF_PREFIX="ENTER_VCF_PREFIX_NAME"
 # ----------------------------------------------------------------------- #
 
 # Load modules
@@ -23,15 +25,17 @@ module load gatk/4.3.0.0
 module load tabix/1.16
 module load bcftools/1.16
 
+# Change directory to snp call output directory
+cd ${PROJECT_FOLDER}/05_filtering
+
 # Filter out loci with singletons (1 HET) or doubletons (1 HOMO ALT) 
-bcftools view -i 'COUNT(GT="het") > 1 || COUNT(GT="AA") > 1' ${VCF_FULL_PATH_NAME} \
---output ${dirname $VCF_FULL_PATH_NAME}/${basename $VCF_FULL_PATH_NAME .vcf.gz}_noprivall.vcf.gz \
+bcftools view -i 'COUNT(GT="het") > 1 || COUNT(GT="AA") > 1' ${VCF_PREFIX}.vcf.gz \
+--output ${VCF_PREFIX}_noprivall.vcf.gz \
 --output-type z
 
 # Index file
 gatk --java-options "-Djava.io.tmpdir=tmp -Xmx90G" IndexFeatureFile \
-   -I ${dirname $VCF_FULL_PATH_NAME}/${basename $VCF_FULL_PATH_NAME .vcf.gz}_noprivall.vcf.gz
+   -I ${VCF_PREFIX}_noprivall.vcf.gz
 
 # Generate vcf statistics
-bcftools stats -s - \
-${dirname $VCF_FULL_PATH_NAME}/${basename $VCF_FULL_PATH_NAME .vcf.gz}_noprivall.vcf.gz > ${dirname $VCF_FULL_PATH_NAME}/${basename $VCF_FULL_PATH_NAME .vcf.gz}_noprivall.stats
+bcftools stats -s - ${VCF_PREFIX}_noprivall.vcf.gz > ${VCF_PREFIX}_noprivall.stats
