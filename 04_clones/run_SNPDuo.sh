bfafdba29062edf01792561c8fa0581e3ccab596
Alice Fornasiero <fornasiero.ali@gmail.com>
2026-03-02 11:10:23 +0100

    Refactor run_SNPDuo.sh to use output prefix variable

--- 04_clones/run_SNPDuo.sh
+++ 04_clones/run_SNPDuo.sh
@@ -33,25 +33,27 @@ module load vcftools/0.1.17
 # Change to working directory
 cd $outdir
 
+# Define output prefix
+outprefix=$(basename $infile .vcf.gz)
+
 # Run vcftools to generate .tped and .tfam files as input for SNPduo
 vcftools --gzvcf $infile \
 --plink-tped \
---out $(basename $infile .vcf.gz)
+--out ${outprefix}
 
 # If chromosome names contain characters (e.g. chr01), the vcftools command will convert them into 0 in the first column of the .tped file
 # The second column of the .tped file contains chr:pos info (e.g. chr01:100).
 # Modify the .tped file to get chromosome names in the first column by splitting the second column into the originary chr and position columns
-sed 's/:/\t/g' $(basename $infile .vcf.gz).tped | awk '{FS=OFS="\t"} {$1=""; sub(FS, ""); print}' >  $(basename $infile .vcf.gz)_tmp.tped
+sed 's/:/\t/g' ${outprefix}.tped | awk '{FS=OFS="\t"} {$1=""; sub(FS, ""); print}' >  ${outprefix}_tmp.tped
 
-# Rename tmp file
-mv $(basename $infile .vcf.gz)_tmp.tped $(basename $infile .vcf.gz).tped
+mv ${outprefix}_tmp.tped ${outprefix}.tped
 
 # Chromosome names have to be numbers only. Remove any character (e.g. "chr", "Chr", ...)
-sed -i 's/Chr0//g' $(basename $infile .vcf.gz).tped
-sed -i 's/Chr//g' $(basename $infile .vcf.gz).tped
+sed -i 's/Chr0//g' ${outprefix}.tped
+sed -i 's/Chr//g' ${outprefix}.tped
 
 # Get IBS counts and summary information. Inputs are data.tfam and data.tped. Output files are .count and .summary
-${snpduopath}/snpduo --tfile $(basename $infile .vcf.gz) \
+${snpduopath}/snpduo --tfile ${outprefix} \
 --counts \
 --summary \
---out $(basename $infile .vcf.gz)_IBS
+--out ${outprefix}_IBS
