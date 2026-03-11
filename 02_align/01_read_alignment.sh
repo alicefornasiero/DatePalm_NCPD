1f2eff73f157101a68f68a2ad3691a2c4808911f
Alice Fornasiero <fornasiero.ali@gmail.com>
2026-03-11 11:21:23 +0100

    Refactor variable names in alignment script
    Updated variable names for clarity and consistency.

--- 02_align/01_read_alignment.sh
+++ 02_align/01_read_alignment.sh
@@ -11,13 +11,13 @@
 # Read aligment using BWA mem
 
 # ----------------------------------------------------------------------- #
-# FASTQ_FOLDER is the path to the folder containing the raw fastq files and the sample list
 # PROJECT_FOLDER is the path to the project folder
+# SAMPLE_LIST is a text file with the list of sample prefix, one per line
 # REFERENCE is the path to the reference genome in fasta format
 # MEM_THREADS is the number of threads
 
-FASTQ_FOLDER="ENTER_INPUT_DIRECTORY_PATH"
 PROJECT_FOLDER="ENTER_OUTPUT_DIRECTORY_PATH"
+SAMPLE_LIST="ENTER_FILE_NAME_AND_PATH"
 REFERENCE="ENTER_REFERENCE_PATH_AND_FILE_NAME"
 MEM_THREADS=32
 # ----------------------------------------------------------------------- #
@@ -37,7 +37,7 @@ module load bwa/0.7.17/gnu-12.2.0
 module load samtools/1.16.1
 
 # Generate the array of slurms
-IFS=$'\n' read -d '' -r -a lines < ${FASTQ_FOLDER}/sample_id_list.txt
+IFS=$'\n' read -d '' -r -a lines < ${SAMPLE_LIST}
 ID=${lines[${SLURM_ARRAY_TASK_ID}]}
 
 # Run BWA mem
