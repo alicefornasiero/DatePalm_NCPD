17714e2e69e24fd539b620fdd98c6f05071dc70d
Alice Fornasiero <fornasiero.ali@gmail.com>
2026-03-19 11:34:12 +0100

    Incorporate population data into PCA plots
    Added functionality to read population information from a group file and updated PCA plots to color samples according to population.

--- 05_structure/02_Plot_PCA.R
+++ 05_structure/02_Plot_PCA.R
@@ -8,11 +8,13 @@
 # -------------------------------------------------------------------------------------------------- #
 # indir - Path to the input directory containing the eigenvalues and eigenvectors obtained with Plink
 # inprefix - Prefix name of the .eigenval and .eigenvec input files from Plink
+# groupfile - Tab separated file with two columns: sample name, population (header required)
 # max_overlaps - Numeric value. Exclude text labels when they overlap more than max_overlap text 
 #                labels or data points. Default: 10. No label printed: 0
 
 indir = "/path/to/input/directory/with/eigenvalues/and/eigenvectors"
 inprefix = "input_file_prefix"
+groupfile = "/path/to/text_file"
 max_overlaps = 0
 # -------------------------------------------------------------------------------------------------- #
 
@@ -29,27 +31,29 @@ setwd(indir)
 if (!dir.exists(indir)) stop("Input directory does not exist!")
 
 # Read input files
+# Read eigenvector input file
 pca <- read_table(paste0(inprefix, ".eigenvec"), col_names = TRUE)
 names(pca)[1] <- "sample"
+# Read eigenvalues input file
 eigenval <- scan(paste0(inprefix, ".eigenval"))
-
-# Sort out the individual species and pops
-pca <- pca %>%
-  mutate(spp = case_when(
-    str_detect(sample, "Male-") ~ "Male",
-    TRUE ~ "Female"
-  ))
-
-# Define colors for each sample category
-cols <- c("Male" = "blue", "Female" = "darkorange1")
+# Read file with information of population
+group <- read_table(groupfile, col_names = TRUE)
+colnames(group) <- c("sample", "pop")
 
 # Convert eigenvalues into percentage of variance explained
 pve <- data.frame(PC = 1:10, pve = eigenval/sum(eigenval)*100)
 
-# Plot scree plot
-# Define output file name
-outscree <- paste0(indir, "/", inprefix, "_screeplot.pdf")
+# Add population info to the pca
+pca <- left_join(pca, group, by = "sample")
 
+# Define color palette accordingly to results of population structure analysis
+# Females
+clist <- c("pop1" = "#CC6677", "pop2" = "#332288", "pop3" = "#DDCC77", "pop4" = "#117733", "admix" = "#BBBBBB")
+# Males
+# clist <- c("pop1" = "#EE7733", "pop2" = "#0077BB", "pop3" = "#EE3377", "admix" = "#BBBBBB")
+
+# Plot scree plot
+outscree <- paste0(indir, "/", inprefix, "_screeplot.pdf")
 scree_plot <- ggplot(pve, aes(PC, pve)) + geom_bar(stat = "identity") +
                 scale_x_continuous(expand = c(0, 0), 
                   name = "Principal Components", 
@@ -63,80 +67,74 @@ scree_plot <- ggplot(pve, aes(PC, pve)) + geom_bar(stat = "identity") +
                   panel.grid.minor = element_blank())
 ggsave(outscree, scree_plot)
 
-# Plot eigenvectors of PC1 and PC2
-# Define output file name
-outpca12 <- ifelse(max_overlaps == 0, 
-                   paste0(indir, "/", inprefix, ".PC1-PC2_nolabs.pdf"), 
-                   paste0(indir, "/", inprefix, ".PC1-PC2.pdf"))
-
-pca_plot12 <- ggplot(pca, aes(PC1, PC2, col = spp)) + 
-              geom_point(size = 1.5, alpha = 0.5) +
-              geom_text_repel(label = pca$"sample", 
-                min.segment.length = 0.2, 
-                seed = 42, box.padding = 0.2, 
-                xlim = c(-Inf, Inf), ylim = c(-Inf, Inf), 
-                max.overlaps = max_overlaps, size = 2) + 
-              scale_colour_manual(values = cols, name = NULL) +
+# Plot PC1 and PC2 and color samples according to population
+outpca12 <- paste0(indir, "/", inprefix, ".PC1-PC2_pops.pdf")
+pca_plot12 <- ggplot(pca, aes(PC1, PC2, fill = pop)) +
+              geom_point(size = 3, alpha = 0.8, shape = 21, colour = "black") +
+              geom_text_repel(label = pca$"sample",
+                min.segment.length = 0.2,
+                seed = 42, box.padding = 0.2,
+                xlim = c(-Inf, Inf), ylim = c(-Inf, Inf),
+                max.overlaps = 100, size = 1) +
               geom_vline(aes(xintercept = 0), linetype = "dashed", linewidth = 0.5) +
               geom_hline(aes(yintercept = 0), linetype = "dashed", linewidth = 0.5) +
               scale_x_continuous(name = paste0("PC1 (", signif(pve$pve[1], 3), "%)"),
                 limits = c(min(pca$PC1)*1, max(pca$PC1)*1)) +
               scale_y_continuous(name = paste0("PC2 (", signif(pve$pve[2], 3), "%)"),
                 limits = c(min(pca$PC2)*1, max(pca$PC2)*1)) +
+              scale_fill_manual(values = clist,
+                name = "Populations") +
               theme(axis.text = element_text(size = 12),
                 axis.title = element_text(size = 14),
                 panel.grid.minor = element_blank(),
-                legend.text = element_text(size = 14))
+                legend.text = element_text(size = 12),
+                legend.title = element_text(size = 14))
 ggsave(outpca12, pca_plot12, width = 7, height = 7, units = "in")
 
-# Plot  eigenvectors of PC1 and PC3 
-# Define output file name
-outpca13 <- ifelse(max_overlaps == 0, 
-                   paste0(indir, "/", inprefix, ".PC1-PC3_nolabs.pdf"), 
-                   paste0(indir, "/", inprefix, ".PC1-PC3.pdf"))
-
-pca_plot13 <- ggplot(pca, aes(PC1, PC3, col = spp)) + 
-              geom_point(size = 1.5, alpha = 0.5) +
-              geom_text_repel(label = pca$"sample", 
-                min.segment.length = 0.2, 
-                seed = 42, box.padding = 0.2, 
-                xlim = c(-Inf, Inf), ylim = c(-Inf, Inf), 
-                max.overlaps = max_overlaps, size = 2) +
-              scale_colour_manual(values = cols, name = NULL) +
+# Plot PC1 and PC3 and color samples according to population
+outpca13 <- paste0(indir, "/", inprefix, ".PC1-PC3_pops.pdf")
+pca_plot13 <- ggplot(pca, aes(PC1, PC3, fill = pop)) +
+              geom_point(size = 3, alpha = 0.8, shape = 21, colour = "black") +
+              geom_text_repel(label = pca$"sample",
+                min.segment.length = 0.2,
+                seed = 42, box.padding = 0.2,
+                xlim = c(-Inf, Inf), ylim = c(-Inf, Inf),
+                max.overlaps = 100, size = 1) +
               geom_vline(aes(xintercept = 0), linetype = "dashed", linewidth = 0.5) +
               geom_hline(aes(yintercept = 0), linetype = "dashed", linewidth = 0.5) +
               scale_x_continuous(name = paste0("PC1 (", signif(pve$pve[1], 3), "%)"),
                 limits = c(min(pca$PC1)*1, max(pca$PC1)*1)) +
               scale_y_continuous(name = paste0("PC3 (", signif(pve$pve[3], 3), "%)"),
                 limits = c(min(pca$PC3)*1, max(pca$PC3)*1)) +
+              scale_fill_manual(values = clist,
+                name = "Populations") +
               theme(axis.text = element_text(size = 12),
                 axis.title = element_text(size = 14),
                 panel.grid.minor = element_blank(),
-                legend.text = element_text(size = 14))
+                legend.text = element_text(size = 12),
+                legend.title = element_text(size = 14))
 ggsave(outpca13, pca_plot13, width = 7, height = 7, units = "in")
 
-# Plot eigenvectors of PC2 and PC3 
-# Define output file name
-outpca23 <- ifelse(max_overlaps == 0, 
-                   paste0(indir, "/", inprefix, ".PC2-PC3_nolabs.pdf"), 
-                   paste0(indir, "/", inprefix, ".PC2-PC3.pdf"))
-
-pca_plot23 <- ggplot(pca, aes(PC2, PC3, col = spp)) + 
-              geom_point(size = 1.5, alpha = 0.5) +
-              geom_text_repel(label = pca$"sample", 
-                min.segment.length = 0.2, 
-                seed = 42, box.padding = 0.2, 
-                xlim = c(-Inf, Inf), ylim = c(-Inf, Inf), 
-                max.overlaps = max_overlaps, size = 2) +
-              scale_colour_manual(values = cols, name = NULL) +
+# Plot PC2 and PC3 and color samples according to population
+outpca23 <- paste0(indir, "/", inprefix, ".PC2-PC3_pops.pdf")
+pca_plot23 <- ggplot(pca, aes(PC2, PC3, fill = pop)) +
+              geom_point(size = 3, alpha = 0.8, shape = 21, colour = "black") +
+              geom_text_repel(label = pca$"sample",
+                min.segment.length = 0.2,
+                seed = 42, box.padding = 0.2,
+                xlim = c(-Inf, Inf), ylim = c(-Inf, Inf),
+                max.overlaps = 100, size = 1) +
               geom_vline(aes(xintercept = 0), linetype = "dashed", linewidth = 0.5) +
               geom_hline(aes(yintercept = 0), linetype = "dashed", linewidth = 0.5) +
               scale_x_continuous(name = paste0("PC2 (", signif(pve$pve[2], 3), "%)"),
                 limits = c(min(pca$PC2)*1, max(pca$PC2)*1)) +
               scale_y_continuous(name = paste0("PC3 (", signif(pve$pve[3], 3), "%)"),
                 limits = c(min(pca$PC3)*1, max(pca$PC3)*1)) +
+              scale_fill_manual(values = clist,
+                name = "Populations") +
               theme(axis.text = element_text(size = 12),
                 axis.title = element_text(size = 14),
                 panel.grid.minor = element_blank(),
-                legend.text = element_text(size = 14))
+                legend.text = element_text(size = 12),
+                legend.title = element_text(size = 14))
 ggsave(outpca23, pca_plot23, width = 7, height = 7, units = "in")
